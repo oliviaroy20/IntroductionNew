d1f68394e1c3b15fe2772db67216153566629c94
Olivia <Olivia@olivias-mbp-3.dhcp.nd.edu>
2017-09-27 18:11:34 -0400

    initial commit

---
+++ src/calculator/SimpleCalculator.java
@@ -0,0 +1,29 @@
+package calculator;
+
+public class SimpleCalculator implements ICalculator {
+
+	@Override
+	public double add(double a, double b) {
+		// TODO Auto-generated method stub
+		return a+b;
+	}
+
+	@Override
+	public double sub(double a, double b) {
+		// TODO Auto-generated method stub
+		return a-b;
+	}
+
+	@Override
+	public double div(double a, double b) {
+		// TODO Auto-generated method stub
+		return a/b;
+	}
+
+	@Override
+	public double mult(double a, double b) {
+		// TODO Auto-generated method stub
+		return a*b;
+	}
+
+}
